ce89400d0261bcc2b6eac418954a8688bc6b2f95
Alinakbdhk <125472323+Alinakbdhk@users.noreply.github.com>
2023-04-28 12:11:36 +0300

    Update user_forms.py

--- project/forms/user_forms.py
+++ project/forms/user_forms.py
@@ -9,10 +9,6 @@ class RegisterForm(FlaskForm):
     password_again = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
     surname = StringField('Surname', validators=[DataRequired()])
     name = StringField('Name', validators=[DataRequired()])
-    age = IntegerField('Age', validators=[DataRequired()])
-    position = StringField('Position', validators=[DataRequired()])
-    speciality = StringField('Speciality', validators=[DataRequired()])
-    address = StringField('Address', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
